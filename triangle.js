d54aa81f67547c13119ece4730fb0a0ae3199393
Pongpol Pipatcheewin <pongpol@Pongpols-MacBook-Air.local>
2025-09-28 10:43:07 +0700

    add bug

--- triangle.js
+++ triangle.js
@@ -8,7 +8,7 @@ function TriangleCalculator(a, b, c) {
 
   if (!Number.isInteger(a) || !Number.isInteger(b) || !Number.isInteger(c)) {
     return "Value is not an integer";
-  } else if (a <= 0 || a > 200 || b <= 0 || b > 200 || c <= 0 || c > 200) {
+  //} else if (a <= 0 || a > 200 || b <= 0 || b > 200 || c <= 0 || c > 200) {
     return "Value is out of range";
   }
   if (a + b <= c || a + c <= b || b + c <= a) {
